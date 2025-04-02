f86911de46915c1696bb4b0e8438cf2800b9915e
CarlosQuintero <196873474+CarlosQuintero8@users.noreply.github.com>
2025-04-02 00:52:34 -0500

    update

--- react-app/src/api/axios.js
+++ react-app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'http://localhost:8081',
+	baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8081', // Usa la variable de entorno o localhost como fallback
 	withCredentials: true,
 	headers: {
 		'Content-Type': 'application/json',
